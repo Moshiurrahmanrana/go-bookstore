83bbee11997d1c85eba2d974ff5b7ed6a0624938
Moshiur Rahman <moshiur@Moshiurs-Mac-mini.local>
2025-05-23 16:40:39 +0600

    Initial commit

---
+++ pkg/config/app.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+
+func Connect() {
+	d, err := gorm.Open("mysql", "moshiur:moshiur@tcp(localhost:3306)/bookstore?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}	
+
+
---
+++ pkg/utils/utils.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func ParseBody(r *http.Request, x interface{}) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	json.Unmarshal(body, x)
+}
